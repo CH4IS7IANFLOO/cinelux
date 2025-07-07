36219a71b553bb154408805b3f29931a31940393
christian <chistiancarreal215@gmail.com>
2025-07-07 08:03:15 +0700

    este archivo es para manejo de sesiones

--- cine/sistema_login/logout.php
+++ cine/sistema_login/logout.php
@@ -1,5 +1,6 @@
 <?php
-session_start();
+session_start();    // Inicia la sesión
+// Elimina todas las variables de sesión
 session_unset();   // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
 header("Location: ../index.php"); // Redirige al index (ajusta la ruta si está en otra carpeta)
